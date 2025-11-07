10334c46deb89fde899ac1c0a52d214c8052099d
Jonnathan Peña <razaisger@hotmail.com>
2025-11-07 06:41:22 -0500

    Testing text input

---
+++ cypress/e2e/max-character-input.cy.js
@@ -0,0 +1,15 @@
+describe('Max Character Input Test', () => {
+  it('Should display the proppiate remaining number of characters count', () => {
+    cy.visit('http://localhost:5173/example-2');
+    cy.get('span').invoke('text').should('eq', '15');
+    cy.get('input').type('Hello');
+    cy.get('span').invoke('text').should('eq', '10');
+    cy.get('input').type('Hello');
+    cy.get('span').invoke('text').should('eq', '5');
+    cy.get('input').type('Hello');
+    cy.get('span').invoke('text').should('eq', '0');
+    cy.get('input').type('Hello');
+    cy.get('input').should('have.value', 'HelloHelloHello');
+    cy.get('span').invoke('text').should('eq', '0');
+  })
+})
