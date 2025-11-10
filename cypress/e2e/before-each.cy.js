46bfa7dcda38770d4036a6de938ae62e29c6b025
Jonnathan Peña <razaisger@hotmail.com>
2025-11-10 06:29:23 -0500

    Using before-each

---
+++ cypress/e2e/before-each.cy.js
@@ -0,0 +1,25 @@
+ describe('Max Character Input Test', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/example-3');
+
+    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').as('fnCharsLeft');
+    cy.get('#first-name-input').as('firstNameInput');
+  });
+
+  it('Should display the proppiate remaining number of characters count with 5 characters', () => {
+    cy.get('@fnCharsLeft').should('eq', '15');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@fnCharsLeft').should('eq', '10');
+  })
+
+  it('Should display the proppiate remaining number of characters count with 10 characters', () => {
+    cy.get('@firstNameInput').type('HelloHello');
+    cy.get('@fnCharsLeft').should('eq', '5');
+  })
+
+  it('Should display the proppiate remaining number of characters count and prevents character input when full', () => {
+    cy.get('@firstNameInput').type('HelloHelloHello ');
+    cy.get('@firstNameInput').should('have.value', 'HelloHelloHello');
+    cy.get('@fnCharsLeft').should('eq', '0');
+  })
+})
