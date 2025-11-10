008697e77a9e04cd294e697ed5eca3eea86a093b
Jonnathan Peña <razaisger@hotmail.com>
2025-11-10 06:12:24 -0500

    Element aliasing

--- cypress/e2e/selecting.cy.js
+++ cypress/e2e/selecting.cy.js
@@ -1,15 +1,19 @@
 describe('Max Character Input Test', () => {
   it('Should display the proppiate remaining number of characters count', () => {
     cy.visit('http://localhost:5173/example-3');
-    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').should('eq', '15');
-    cy.get('#first-name-input').type('Hello');
-    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').should('eq', '10');
-    cy.get('#first-name-input').type('Hello');
-    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').should('eq', '5');
-    cy.get('#first-name-input').type('Hello');
-    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').should('eq', '0');
-    cy.get('#first-name-input').type('Hello');
-    cy.get('#first-name-input').should('have.value', 'HelloHelloHello');
-    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').should('eq', '0');
+
+    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').as('fnCharsLeft');
+    cy.get('#first-name-input').as('firstNameInput');
+
+    cy.get('@fnCharsLeft').should('eq', '15');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@fnCharsLeft').should('eq', '10');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@fnCharsLeft').should('eq', '5');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@fnCharsLeft').should('eq', '0');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@firstNameInput').should('have.value', 'HelloHelloHello');
+    cy.get('span[data-cy="first-name-chars-left-count"]').should('eq', '0');
   })
 })
