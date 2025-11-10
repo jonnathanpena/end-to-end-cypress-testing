021325f0e2046936380d8656e380f5326df28836
Jonnathan Peña <razaisger@hotmail.com>
2025-11-10 06:21:08 -0500

    Command results example

---
+++ cypress/e2e/results.cy.js
@@ -0,0 +1,22 @@
+describe('Max Character Input Test', () => {
+  it('Should display the proppiate remaining number of characters count', () => {
+    cy.visit('http://localhost:5173/example-3');
+
+    cy.get('span[data-cy="first-name-chars-left-count"]').then(function($charactersLeftSpan) {
+      expect($charactersLeftSpan.text()).to.equal('15');
+    });
+
+    cy.get('span[data-cy="first-name-chars-left-count"]').invoke('text').as('fnCharsLeft');
+    cy.get('#first-name-input').as('firstNameInput');
+
+    cy.get('@fnCharsLeft').should('eq', '15');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@fnCharsLeft').should('eq', '10');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@fnCharsLeft').should('eq', '5');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@fnCharsLeft').should('eq', '0');
+    cy.get('@firstNameInput').type('Hello');
+    cy.get('@firstNameInput').should('have.value', 'HelloHelloHello');
+  })
+})
